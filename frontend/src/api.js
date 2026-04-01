e9f89c9b1ccdcc73fffad63f6fd84f0004ce9afb
PardeepRattan0012 <prdprattan@gmail.com>
2026-04-02 01:18:13 +0530

    final API config

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from './store/authStore';
 
 const api = axios.create({
-  baseURL: "https://syallabus-management-system.onrender.com/api"
+  baseURL: "https://syallabus-management-system.onrender.com"
 });
 
 api.interceptors.request.use(config => {
